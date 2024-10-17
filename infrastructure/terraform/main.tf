901bfe8541a1bd5b2bbdb817002e5e0a501f9001
Jürgen Droste <juergen.droste@telefonica.com>
2024-10-17 16:41:07 +0200

    infra: aws-dynamo table

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -21,3 +21,15 @@ resource "aws_s3_bucket_versioning" "terraform-state" {
   }
 }
 
+resource "aws_dynamodb_table" "state_lock_table" {
+  name         = "terraform_state_lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
+
