7704aafe4364debce599f299b46add30c93375ee
Jürgen Droste <juergen.droste@telefonica.com>
2024-10-30 15:05:13 +0100

    infra: role for cluster

---
+++ infrastructure/terraform/ecs_arn_role.tf
@@ -0,0 +1,29 @@
+# IAM Rolle für ECS Task Execution erstellen
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ecs-tasks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Berechtigungen an die Execution Role anhängen
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# Optional: Berechtigungen für Zugriff auf ECR (falls benötigt)
+resource "aws_iam_role_policy_attachment" "ecr_read_access" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
---
+++ infrastructure/terraform/variables.tf
@@ -0,0 +1,3 @@
+data "aws_vpc" "pond_vpc" {
+  id = "vpc-009e668fa4cb4d349"  # ID der Sandbox vpc
+}
